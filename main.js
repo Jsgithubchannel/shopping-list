dff9072b89bed074d39b5985c76e81cdcd974f2b
JS <jsschannel99@gmail.com>
2020-07-15 01:57:47 +0900

    JS: Unresolved problem with a trash bin

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const input = document.querySelector(".input");
 const addItemBtn = document.querySelector("#addItemBtn");
+const list = document.querySelector("ul");
+//const trashBinIcon = document.querySelector(".fa-trash");
 
 // Add a stuff with a click btn or the Enter key
 addItemBtn.addEventListener("click", () => {
@@ -9,11 +11,30 @@ document.addEventListener("keyup", (event) => {
   if (event.key === "Enter") newElement();
 });
 
+//Remove the stuff clicking a trash bin icon
+// trashBinIcon.addEventListener("click", () => {
+//   removeElement();
+// });
+
 function newElement() {
-  let list = document.querySelector("ul");
+  //add stuffs
   let li = document.createElement("li");
   li.textContent = input.value;
   input.value = "";
-  let appendedItem = list.appendChild(li);
-  console.log("success");
+  list.appendChild(li);
+
+  //add a trash bin icon
+
+  //   let btn = document.createElement("button");
+  //   btn.setAttribute("class", "fa fa-trash");
+
+  //let btn = document.createElement("button");
+  //li.innerHTML('<i class="fa fa-trash" aria-hidden="true"></i>');
 }
+
+// function removeElement() {
+//   let li = document.querySelector("li");
+//   list.removeChild(li);
+// }
+
+//해결해야할 것: trash bin이 li의 글자 왼쪽에 나옴
