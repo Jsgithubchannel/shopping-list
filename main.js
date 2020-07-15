b097455309b6c03885c12ea0614d83b50900af4c
JS <jsschannel99@gmail.com>
2020-07-15 15:23:48 +0900

    JS: remove list clicking a trash bin

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const input = document.querySelector(".input");
 const addItemBtn = document.querySelector("#addItemBtn");
 const list = document.querySelector("ul");
-//const trashBinIcon = document.querySelector(".fa-trash");
 
 // Add a stuff with a click btn or the Enter key
 addItemBtn.addEventListener("click", () => {
@@ -11,11 +10,6 @@ document.addEventListener("keyup", (event) => {
   if (event.key === "Enter") newElement();
 });
 
-//Remove the stuff clicking a trash bin icon
-// trashBinIcon.addEventListener("click", () => {
-//   removeElement();
-// });
-
 function newElement() {
   //add stuffs
   let li = document.createElement("li");
@@ -24,17 +18,23 @@ function newElement() {
   list.appendChild(li);
 
   //add a trash bin icon
-
-  //   let btn = document.createElement("button");
-  //   btn.setAttribute("class", "fa fa-trash");
-
-  //let btn = document.createElement("button");
-  //li.innerHTML('<i class="fa fa-trash" aria-hidden="true"></i>');
+  let removeBtn = document.createElement("i");
+  removeBtn.setAttribute("class", "fa fa-trash");
+  li.appendChild(removeBtn);
 }
 
-// function removeElement() {
-//   let li = document.querySelector("li");
-//   list.removeChild(li);
-// }
+//Remove the stuff clicking a trash bin icon
+document.querySelector(".deco").addEventListener("click", function (e) {
+  const tgt = e.target;
+  if (tgt.classList.contains("fa-trash")) tgt.closest("li").remove();
+});
 
-//해결해야할 것: trash bin이 li의 글자 왼쪽에 나옴
+// document.addEventListener("click", function (e) {
+//   if (hasClass(e.target, "fa-trash")) {
+//     let li = document.querySelector("li");
+//     list.removeChild(li);
+//   }
+// });
+// function hasClass(elem, className) {
+//   return elem.className.split(" ").indexOf(className) > -1;
+// }
